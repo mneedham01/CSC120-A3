9bdb3ed56295a6f16f71a3c61443bd65ed99b3d4
mneedham01 <mneedham@smith.edu>
2023-02-13 21:46:39 -0500

    started the conversation

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,15 @@
-class Conversation {
+import java.util.Scanner;
 
+class Conversation {
   public static void main(String[] arguments) {
+
+    Scanner in;
+    String transcript;
+    Short round;
+
+    in=newScanner(System.in);
+    System.out.println("How many rounds?")
+
     // You will start the conversation here.
   }
 }
