0913749ea463465d4d536c15cefae2e134a42c8b
mneedham01 <mneedham@smith.edu>
2023-02-13 21:55:24 -0500

    got it to start printing

--- Conversation.java
+++ Conversation.java
@@ -6,10 +6,25 @@ class Conversation {
     Scanner in;
     String transcript;
     Short round;
+    String response; 
 
-    in=newScanner(System.in);
-    System.out.println("How many rounds?")
+    //asking for the number of rounds 
+    in= new Scanner(System.in);
+    System.out.print("How many rounds? ");
+    round=in.nextShort();
+
+    //start off conversation
+    System.out.println("\nHello! What's on your mind?");
+
+    //loop
+    //for (int i=0; i< round-1; i++){
+       // in= new Scanner(System.in);
+       // response=in.nextString;
+
+
+    }
 
     // You will start the conversation here.
-  }
 }
+
+
