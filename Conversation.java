c38fb69576ebe7027996949478c2633373fba03e
mneedham01 <mneedham@smith.edu>
2023-02-14 19:40:48 -0500

    made some progress in basic checking

--- Conversation.java
+++ Conversation.java
@@ -6,7 +6,6 @@ class Conversation {
     Scanner in;
     String transcript="\nHello! What's on your mind?";
     Short round;
-    String original_user_response=""; 
 
     //asking for the number of rounds 
     in= new Scanner(System.in);
@@ -25,67 +24,36 @@ class Conversation {
       //adds it to the transcript
       transcript= transcript+user_response+"\n";
 
-      //check if the response has "I am" or "You are"
-      if (user_response.contains("I am")){
-        user_response=user_response.replace("I am","I'm");
-      }
-      if (user_response.contains("You are")){
-        user_response= user_response.replace("You are","You're");
-      }
-  
-      //split response into words
-      String[] words= original_user_response.split(" ");
+      String computer_response="Mmm hmmm.";
 
-      //check whether it has the words we want and add replacements to computer_response
-      String computer_response="";
-      
-      for (String token:words){
-        System.out.println(token);
-        //check if it says "I"
-        if (token.equals("I")){
-          //if it's at the start of the sentence, then capitalize. If not, lowercase.
-          if (token.equals(words[0])){
-              token="You";
-          }
-          else{
-            token="you";
-          }
-        }
-        if (token.equals("I'm")){
-          if (token.equals(words[0])){
-            token="You're";
-          }
-          else{
-            token="you're";
-          }
-        if (token.equals("You")){
-          token="I";
-        }
-        if (token.equals("You're")){
-          token="I'm";
-        }
-        computer_response+=token;
-
-        }
-        }
-        //print response
-        System.out.println("I got out of the for loop");
-        System.out.println(computer_response);
+      if (user_response.contains("I ")){
+        computer_response=user_response.replace("I ","you ")+"?";
+      }
+      if (user_response.contains("I'm ")){
+        computer_response=user_response.replace("I'm ","you're ")+"?";
+      }
+      if (user_response.contains("you ")){
+        computer_response=user_response.replace("you","I")+"?";
       }
 
-      //if (i!=0){
-       // System.out.println("Mmm hmm.");
-       // transcript=transcript+"Mmm hmm. \n";
+      System.out.println(computer_response);
+
+     //if (i!=0){
+      // System.out.println("Mmm hmm.");
+      // transcript=transcript+"Mmm hmm. \n";
      // }
 
-     // if (i==round){
-       // System.out.println("\nConversation finished.\nTranscript:");
-      //  System.out.println(transcript);
-   //   }
+     if (i==round){
+       System.out.println("\nConversation finished.\nTranscript:");
+       System.out.println(transcript);
+     }
 
-       }
-
+      }
     }
+  }
+
+
+  
 
     // You will start the conversation here.
 
