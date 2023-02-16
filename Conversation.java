035d44ba09cc8332e648677217051950b8f2362d
mneedham01 <mneedham@smith.edu>
2023-02-15 23:40:47 -0500

    added javadoc

--- Conversation.java
+++ Conversation.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.Random;
 
-class Conversation {
-  public static void main(String[] arguments) {
+/**
+ * Starts a conversation with user. Will mirror back certain words or ask a question. 
+ */
 
+class Conversation {
+  /**
+     * The main intiates and executes the program.
+     */
+  public static void main(String[] arguments) {
     Scanner in;
     ArrayList<String> transcript = new ArrayList<String>();
     Short round;
@@ -38,17 +44,23 @@ class Conversation {
       //set up counter for changing mirror words
       int counter=0;
 
+
       //loops through the user response array and checks if it's a word to be mirrored. 
       for (int j=0; j<words.length;j++){
           if (words[j].equals("I")){
             counter += 1;
-            //Checks if it's the first letter to see if it should capitalize
+            //Checks if it's the first lett er to see if it should capitalize
             if (j==0){
               computer_response+="You ";
             }
             else{
               computer_response+="you ";
             }
+            continue;
+          }
+          if (words[j].equals("me")){
+            counter+=1;
+            computer_response+="you ";
             continue;
           }
           if (words[j].equals("I'm")){
@@ -61,6 +73,16 @@ class Conversation {
             }
             continue;
           }
+          if (words[j].equals("am")){
+            counter+=1;
+            computer_response+="are ";
+            continue;
+          }
+          if (words[j].equals("my")){
+            counter+=1;
+            computer_response+="your ";
+            continue;
+          }
           if (words[j].equals("you")){
             counter += 1;
             computer_response+="I ";
@@ -69,6 +91,11 @@ class Conversation {
           if (words[j].equals("you're")){
             counter += 1;
             computer_response+="I'm ";
+            continue;
+          }
+          if (words[j].equals("your")){
+            counter+=1;
+            computer_response+="my ";
             continue;
           }
           else{
@@ -90,7 +117,7 @@ class Conversation {
       if (counter>0 & user_response.length()>0){
         int puncPlace=computer_response.charAt(computer_response.length()-2);
         if (puncPlace=='.'||puncPlace=='!'||puncPlace=='?'||puncPlace==','||puncPlace==','){;
-          computer_response=computer_response.substring(0,computer_response.length()-2);
+          computer_response=computer_response.substring(0,computer_response.length()-1);
           computer_response+="?";
         }
         else{
@@ -104,6 +131,7 @@ class Conversation {
   
       
     }
+    //Close conversation and print transcript.
     System.out.println("\nConversation finished.\nTranscript:");
     System.out.println(transcript);
       }
